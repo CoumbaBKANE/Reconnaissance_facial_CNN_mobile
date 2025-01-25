9590bb4be4dfb5f7b82ce54d805a68b287bf0aa0
coumbakane41 <coumbakane41@gmail.com>
2025-01-25 01:56:51 +0000

    setup url

--- face_recognition_frontend/constantes.js
+++ face_recognition_frontend/constantes.js
@@ -1,2 +1,2 @@
-const API_URL = ' https://43a2-41-82-180-207.ngrok-free.app/';
+const API_URL = ' https://7e06-41-82-180-207.ngrok-free.app/';
 export const EndPoints = { LEARNING: API_URL+'learn', RECOGNIZE: API_URL+'recognize', };
